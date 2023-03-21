53f1de53ffcb64ad4bfc1dc9f73a444711adf432
Vivek <84849703+vt57299@users.noreply.github.com>
2023-03-21 22:57:32 +0530

    Update question1.py

--- question1.py
+++ question1.py
@@ -1,1 +1,36 @@
+# Breadth First Traversal for a graph
 
+from collections import defaultdict
+class graph:
+    def __init__(self):
+        self.data = defaultdict(list)
+
+    def addedge(self,u,v):
+        self.data[u].append(v)
+    
+    def bfs(self,start):
+        visited = [False] * (max(self.data)+1)
+        queue = []
+
+        queue.append(start)
+        visited[start] = True
+
+        while queue:
+            start= queue.pop(0)
+            print(start,end=" ")
+        
+            for i in self.data[start]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i]  = True
+
+
+obj = graph()
+obj.addedge(0,1)
+obj.addedge(0,2)
+obj.addedge(1,2)
+obj.addedge(2,0)
+obj.addedge(2,3)
+obj.addedge(3,3)
+
+obj.bfs(2)
